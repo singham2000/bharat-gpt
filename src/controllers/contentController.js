676714391c4bfa0340811d9fa1135aa2631ac032
singham2000 <thakursuryapratap9@gmail.com>
2024-07-01 15:29:25 +0530

    updates

--- src/controllers/contentController.js
+++ src/controllers/contentController.js
@@ -27,7 +27,7 @@ exports.getContent = catchAsyncError(async (req, res, next) => {
           if (err) {
             res.status(400).json({
               success: false,
-              message: "An error has occurred",
+              message: "An error has occurred"+err,
             });
           } else {
             res.status(200).json({
@@ -43,7 +43,7 @@ exports.getContent = catchAsyncError(async (req, res, next) => {
         if (err) {
           res.status(400).json({
             success: false,
-            message: "An error has occurred",
+            message: "An error has occurred"+err,
           });
         } else {
           res.status(200).json({
